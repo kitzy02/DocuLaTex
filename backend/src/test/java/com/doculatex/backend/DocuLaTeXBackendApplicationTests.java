11b2e316d68e3ebc2c47ee0737db6562b5ad613d
kitzy02 <142169510+kitzy02@users.noreply.github.com>
2026-02-18 15:03:44 +0530

    Setup Backend

---
+++ backend/src/test/java/com/doculatex/backend/DocuLaTeXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doculatex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuLaTeXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
